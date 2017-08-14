d4e18e3b1d830cb141a791ae0dedbf4aba5ba895
jimmy dhiu <dhiu.jim@gmail.com>
2017-08-14 18:43:58 +0200

    Add create a boat

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,25 @@
 class BoatsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :show]
+
+  def new
+     @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.save
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :description, :category_id, :address, :capacity, :price, :caution, :size, :horse_power)
+  end
+
+
 end
