7aa56e68f748ae3b831faf7ad246990181e7af2f
jorge ruivinho <jorge.ruivinho@me.com>
2017-08-15 15:42:55 +0200

    list current user bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,27 @@
 class BookingsController < ApplicationController
 
+  def index
+    @bookings = Booking.where(user_id: current_user)
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 
 
 end
