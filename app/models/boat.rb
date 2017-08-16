357c5139764cfe6bba55373cb1e7ba881874d8a2
af <a.fabiano@live.fr>
2017-08-16 15:32:25 +0200

    add autocomplete

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,7 +3,8 @@ class Boat < ApplicationRecord
   belongs_to :category
   has_many :boat_options
 
-
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
   def self.search(search)
     where("address LIKE ?", "%#{search}%")
