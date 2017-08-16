0b23f9c4121a2f7919666e76d24318e8e66d02f9
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-16 14:38:00 +0200

    create users (client & host with boat) & options, booking, catégories, boat_options

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,20 @@ puts "Destroy all Categories"
 puts "----------------------"
 
 
+voilier = Category.create(name: "Voilier")
+moteur = Category.create(name: "Moteur")
+categories = [voilier, moteur]
+puts "Categories created !"
+puts "--------------------"
 
-
+options_string = ["frigo", "coque amovible", "turbo boost", "bouteille de champagne",
+"four", "équipe de rameur", "Laure Manaudou"]
+options_object = []
+options_string.each do |option|
+  options_object << Option.create(name: options_string.sample)
+end
+puts "Options created !"
+puts "-----------------"
 
 
 100.times do
@@ -30,10 +42,30 @@ puts "----------------------"
   p user
   if user.status == "host"
     boat = Boat.create(name: Faker::DragonBall.character, description: Faker::Lorem.sentence,
-                      user: user)
+                      user: user,
+                      address: ["Arcachon", "Biarritz", "Lorient", "La Rochelle"].sample,
+                      capacity: rand(2..15),
+                      price: rand(150..500),
+                      caution: rand(10..50) * 100,
+                      size: rand(3..20),
+                      horse_power: rand(9..300),
+                      photo: Faker::Placeholdit.image,
+                      category: categories.sample )
+    rand(3..options_string.size).times do
+      BoatOption.create(boat: boat, option: options_object.sample)
+    end
+
     p boat
+  else
+    boats = Boat.all
+    boat = Boat.find(rand(1..boats.size))
+
+    Booking.create(user: user, boat: boat)
   end
+
 end
+
+puts "$$$$$$$$$$$$$$$$$ YEAH !!! $$$$$$$$$$$$$$$$$$$$$"
 
 
 
