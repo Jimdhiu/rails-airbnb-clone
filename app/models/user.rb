d20cfa5f07b8e401e4fce0f03957f66d12178e11
jorge ruivinho <jorge.ruivinho@me.com>
2017-08-17 15:04:30 +0200

    add profile picture to navbar only visible on production

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :boats, dependent: :destroy
   has_many :bookings
 
+
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
