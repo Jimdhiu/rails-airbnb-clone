8820049bcf6290ce000f98ac1dd4927bfb64d9e0
jimmy dhiu <dhiu.jim@gmail.com>
2017-08-18 15:45:42 +0200

    add css booking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def image_or_default image, options = {}
+    if image.nil?
+      image_path "no_boat_photo.png"
+    else
+      cl_image_path image.path, options
+    end
+  end
 end
